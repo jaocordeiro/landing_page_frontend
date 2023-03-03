82a0c3784606edbcc3975ab7c014baf4b0857f64
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-03-03 16:54:45 -0300

    feat: created component Menu part 2

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -2,8 +2,13 @@ import styled, { css } from 'styled-components';
 import { Container as SectionContainer } from '../SectionContainer/styles';
 import { Title as Heading } from '../Heading/styles';
 
+const menuVisible = () => css`
+  visibility: visible;
+  opacity: 1;
+`;
+
 export const Container = styled.div`
-  ${({ theme }) => css`
+  ${({ theme, visible }) => css`
       position: fixed;
       z-index: 5;
       top: 0;
@@ -12,6 +17,7 @@ export const Container = styled.div`
       width: 100%;
       border-bottom: ${theme.colors.mediumGray};
       background: ${theme.colors.white};
+      transition: all 300ms ease-in-out;
 
       > ${SectionContainer} {
         padding-top: 0;
@@ -25,6 +31,9 @@ export const Container = styled.div`
 
       @media ${theme.media.lteMedium} {
       height: 100vh;
+      visibility: hidden;
+      opacity: 0;
+      ${visible && menuVisible(theme)}
 
       > ${SectionContainer} {
         display: grid;
@@ -59,7 +68,29 @@ export const MenuContainer = styled.div`
 `;
 
 export const Button = styled.button`
-  ${() => css`
+  ${({ theme, visible }) => css`
+      z-index: 6;
+      position: fixed;
+      top: 2rem;
+      right: 2rem;
+      width: 4rem;
+      height: 4rem;
+      background: ${theme.colors.primaryColor};
+      color: ${theme.colors.white};
+      border-radius: 7px;
+      border: none;
+      display: none;
+      pointer-events: ${visible ? 'none' : 'all'};
 
+      @media ${theme.media.lteMedium} {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+      > svg {
+        width: 3rem;
+        height: 3rem;
+      }
   `}
 `;

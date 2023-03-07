48346896d39f770f3e71a3300cefa72d8b5d6718
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-03-07 12:06:50 -0300

    feat: created component GridText

---
+++ src/components/GridText/mock.js
@@ -0,0 +1,23 @@
+export default {
+  background: false,
+  title: 'My grid',
+  description:
+    'Lorem ipsum dolor sit, amet elit. Debitis cum delectus molestias, non commodi maxime distinctio unde.',
+  grid: [
+    {
+      title: 'Teste 1',
+      description:
+        'Lorem ipsum dolor sit, amet elit. Debitis cum delectus molestias, non commodi maxime distinctio unde,  non commodi maxime distinctio unde.',
+    },
+    {
+      title: 'Teste 2',
+      description:
+        'Lorem ipsum dolor sit, amet elit. Debitis cum delectus molestias, non commodi maxime distinctio unde,  non commodi maxime distinctio unde.',
+    },
+    {
+      title: 'Teste 3',
+      description:
+        'Lorem ipsum dolor sit, amet elit. Debitis cum delectus molestias, non commodi maxime distinctio unde,  non commodi maxime distinctio unde.',
+    },
+  ],
+};

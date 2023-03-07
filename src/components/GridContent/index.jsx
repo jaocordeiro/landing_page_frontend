9d6ac026798f61508c731681a928d0edd149a6bc
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-03-06 22:06:13 -0300

    feat: created component GridContent

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,26 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionBackground } from '../SectionBackground';
+import { TextComponent } from '../TextComponent';
+import { Heading } from '../Heading';
+
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <Styled.Html>
+          <TextComponent>{html}</TextComponent>
+        </Styled.Html>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  background: P.bool,
+};
